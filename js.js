73118d807a6378b3e318e943c7bc82fe3f4b596d
Luciana-Cuenca <luciana.cuenca@campusucc.edu.co>
2026-02-18 20:44:51 -0500

    Agrega modo oscuro y dato curioso auuuu

--- js.js
+++ js.js
@@ -1,3 +1,17 @@
-console.log("hello world :v ");
+// botón modo oscuro
+const btn = document.createElement("button");
+btn.textContent = "Cambiar modo";
+document.body.prepend(btn);
 
-alert ("welcome to javascript");
+btn.addEventListener("click", ()=>{
+    document.body.classList.toggle("dark");
+});
+
+// interacción
+const info = document.createElement("p");
+info.textContent = "Haz clic para saber un dato curioso";
+document.body.appendChild(info);
+
+info.addEventListener("click", ()=>{
+    info.textContent = "Los lobos pueden recorrer más de 50 km al día.";
+})
